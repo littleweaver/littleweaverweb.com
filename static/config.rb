750a4cc1a8c01902f4b2601f2039cf72af976e50
Harris Lapiroff <harris@chromamine.com>
2014-02-19 16:05:02 -0500

    Created a single temporary informational page.

--- static/config.rb
+++ static/config.rb
@@ -2,7 +2,6 @@
 require 'bootstrap-sass'
 
 # Set this to the root of your project when deployed:
-http_path = "/"
 css_dir = "css"
 sass_dir = "sass"
 images_dir = "images"
@@ -12,7 +11,7 @@ javascripts_dir = "javascripts"
 # output_style = :expanded or :nested or :compact or :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
