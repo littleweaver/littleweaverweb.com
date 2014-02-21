2a05c3eb0fa32d2422db57cd5a734e7f82c31e90
Harris Lapiroff <harris@chromamine.com>
2014-02-20 22:53:52 -0500

    Cleaned up config.rb.

--- static/config.rb
+++ static/config.rb
@@ -1,19 +1,10 @@
-# Require any additional compass plugins here.
 require 'bootstrap-sass'
 
-# Set this to the root of your project when deployed:
 css_dir = "css"
 sass_dir = "sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
-
-# You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
-
-# To enable relative paths to assets via compass helper functions. Uncomment:
+output_style = :expanded
 relative_assets = true
-
-# To disable debugging comments that display the original location of your selectors. Uncomment:
 line_comments = false
-
 preferred_syntax = :sass
