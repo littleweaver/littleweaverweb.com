8689fc355a7faa278b570044569b11814e707978
Cameron Higby-Naquin <me@chigby.org>
2016-02-29 18:08:34 -0500

    Convert {blog,work}page images to captioned images.

--- core/migrations/0027_auto_20160226_2103.py
+++ core/migrations/0027_auto_20160226_2103.py
@@ -8,6 +8,24 @@ import core.blocks
 import wagtail.wagtailimages.blocks
 
 
+def images_to_captioned_images(apps, schema_editor):
+    BlogPage = apps.get_model('core', 'BlogPage')
+    WorkPage = apps.get_model('core', 'WorkPage')
+    db_alias = schema_editor.connection.alias
+    for page in BlogPage.objects.using(db_alias).all():
+        body = page.body
+        for block in body.stream_data:
+            if block.get('type', '') == 'image' and isinstance(block.get('value'), int):
+                block['value'] =  {u'caption': u'', u'image': block['value'], u'is_screenshot': False}
+        page.save()
+    for page in WorkPage.objects.using(db_alias).all():
+        body = page.body
+        for block in body.stream_data:
+            if block.get('type', '') == 'image' and isinstance(block.get('value'), int):
+                block['value'] =  {u'caption': u'', u'image': block['value'], u'is_screenshot': False}
+        page.save()
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -19,5 +37,13 @@ class Migration(migrations.Migration):
             model_name='workpage',
             name='body',
             field=wagtail.wagtailcore.fields.StreamField([(b'rich_text', wagtail.wagtailcore.blocks.RichTextBlock(label=b'Rich Text', icon=b'doc-full')), (b'code', wagtail.wagtailcore.blocks.StructBlock([(b'language', wagtail.wagtailcore.blocks.ChoiceBlock(choices=[(b'python', b'Python'), (b'js', b'Javascript'), (b'bash', b'Bash/Shell'), (b'html', b'HTML'), (b'css', b'CSS'), (b'scss', b'SCSS'), (b'php', b'PHP')])), (b'code', wagtail.wagtailcore.blocks.TextBlock())], icon=b'code')), (b'quote', wagtail.wagtailcore.blocks.StructBlock([(b'quote', wagtail.wagtailcore.blocks.RichTextBlock()), (b'credit', wagtail.wagtailcore.blocks.RichTextBlock())], icon=b'openquote')), (b'markdown', core.blocks.MarkdownBlock()), (b'html', wagtail.wagtailcore.blocks.RawHTMLBlock(label=b'HTML', icon=b'site')), (b'image', wagtail.wagtailcore.blocks.StructBlock([(b'image', wagtail.wagtailimages.blocks.ImageChooserBlock()), (b'caption', wagtail.wagtailcore.blocks.RichTextBlock()), (b'is_screenshot', wagtail.wagtailcore.blocks.BooleanBlock())]))]),
+        ),
+        migrations.AlterField(
+            model_name='blogpage',
+            name='body',
+            field=wagtail.wagtailcore.fields.StreamField([(b'rich_text', wagtail.wagtailcore.blocks.RichTextBlock(label=b'Rich Text', icon=b'doc-full')), (b'code', wagtail.wagtailcore.blocks.StructBlock([(b'language', wagtail.wagtailcore.blocks.ChoiceBlock(choices=[(b'python', b'Python'), (b'js', b'Javascript'), (b'bash', b'Bash/Shell'), (b'html', b'HTML'), (b'css', b'CSS'), (b'scss', b'SCSS'), (b'php', b'PHP')])), (b'code', wagtail.wagtailcore.blocks.TextBlock())], icon=b'code')), (b'quote', wagtail.wagtailcore.blocks.StructBlock([(b'quote', wagtail.wagtailcore.blocks.RichTextBlock()), (b'credit', wagtail.wagtailcore.blocks.RichTextBlock())], icon=b'openquote')), (b'markdown', core.blocks.MarkdownBlock()), (b'html', wagtail.wagtailcore.blocks.RawHTMLBlock(label=b'HTML', icon=b'site')), (b'image', wagtail.wagtailcore.blocks.StructBlock([(b'image', wagtail.wagtailimages.blocks.ImageChooserBlock()), (b'caption', wagtail.wagtailcore.blocks.RichTextBlock()), (b'is_screenshot', wagtail.wagtailcore.blocks.BooleanBlock())]))]),
+        ),
+        migrations.RunPython(
+            images_to_captioned_images
         ),
     ]
--- core/models.py
+++ core/models.py
@@ -116,7 +116,7 @@ class ServicesPage(Page):
         ('quote', QuoteBlock(icon='openquote')),
         ('markdown', MarkdownBlock()),
         ('html', blocks.RawHTMLBlock(icon='site', label='HTML')),
-        ('image', ImageChooserBlock()),
+        ('image', CaptionedImageBlock()),
     ])
     banner_image = models.ForeignKey("wagtailimages.Image", null=True, blank=True,
                                      on_delete=models.SET_NULL)
