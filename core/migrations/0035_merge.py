b7248f5f56ffd5c25062a8cb19fffec5094e6da9
Harris Lapiroff <harris@chromamine.com>
2016-03-02 21:28:49 -0500

    Merge migrations

---
+++ core/migrations/0035_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0034_auto_20160302_1450'),
+        ('core', '0031_auto_20160301_1830'),
+    ]
+
+    operations = [
+    ]
