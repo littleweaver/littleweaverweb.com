3ca540cbae3a1246c6544999a1c406add68aff9c
Stephen Burrows <stephen.r.burrows@gmail.com>
2016-03-16 11:09:15 -0700

    Added specific browser support to autoprefixer.

--- littleweaverweb/settings/base.py
+++ littleweaverweb/settings/base.py
@@ -131,6 +131,19 @@ COMPRESS_PRECOMPILERS = (
 COMPRESS_CSS_FILTERS = (
     'django_compressor_autoprefixer.AutoprefixerFilter',
 )
+# Based on: https://github.com/twbs/bootstrap/blob/v4.0.0-alpha.2/Gruntfile.js#L24
+_browsers = [
+    'Chrome >= 35',
+    'Firefox >= 31',
+    'Edge >= 12',
+    'Explorer >= 9',
+    'iOS >= 8',
+    'Safari >= 8',
+    'Android 2.3',
+    'Android >= 4',
+    'Opera >= 12',
+]
+COMPRESS_AUTOPREFIXER_ARGS = '--use autoprefixer --autoprefixer.browsers "{}"'.format(','.join(_browsers))
 
 
 # Template configuration
