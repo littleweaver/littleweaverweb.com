1f7442498c32a9922c27959ef49a44467ccb6b75
Stephen Burrows <stephen.r.burrows@gmail.com>
2016-05-09 21:12:26 -0700

    Switched to sendgrid.

--- salt/webserver/webproject/webproject/settings.py
+++ salt/webserver/webproject/webproject/settings.py
@@ -48,8 +48,8 @@ DATABASES = {
 
 DEFAULT_FROM_EMAIL = '{{ pillar["deploy"]["default_from_email"] }}'
 SERVER_EMAIL = '{{ pillar["deploy"]["server_email"] }}'
-MANDRILL_API_KEY = '{{ pillar["deploy"]["mandrill_api_key"] }}'
-EMAIL_BACKEND = "djrill.mail.backends.djrill.DjrillBackend"
+SENDGRID_API_KEY = '{{ pillar["deploy"]["sendgrid_api_key"] }}'
+EMAIL_BACKEND = "sgbackend.SendGridBackend"
 
 GOOGLE_ANALYTICS_UA = 'UA-52154832-3'
 GOOGLE_ANALYTICS_DOMAIN = 'littleweaverweb.com'
